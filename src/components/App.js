c474c1208b851fe57e513b7e9bf8c425a865bfad
Yu Shi <yushiys95@gmail.com>
2021-04-05 13:52:01 -0400

    commit tree cdbad1a3c2c251ea4d31a77d19e2570546458f0d

--- src/components/App.js
+++ src/components/App.js
@@ -16,12 +16,20 @@ function Home() {
   );
 }
 
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* write an <About> component here */}
+      <Home />
+      <About /> 
     </div>
   );
 }
